89e8a3391064d9d87323682eccdc62cea4f9f673
zhaodongchao <997546127@qq,com>
2016-06-29 19:19:35 +0800

    测试时用于初始化Customer表的sql脚本

---
+++ src/main/resources/init.sql
@@ -0,0 +1,4 @@
+TRUNCATE customer ;
+INSERT INTO customer (id,name,contact,telephone,email,remark) VALUES (1,'customer1','Jack','18221040570','997546127@qq.com','is a test');
+INSERT INTO customer (id,name,contact,telephone,email,remark) VALUES (2,'customer2','Rose','18221040570','997546127@qq.com','is a test');
+INSERT INTO customer (id,name,contact,telephone,email,remark) VALUES (3,'中国移动','李晓英','12345678902','666666666@qq.com','大单客户');
